4fa3914211d27975b371059399dc8094c9dcc3e9
John R Iversen <john@johniversen.org>
2011-09-26 20:26:49 -0700

    Initial Commit

---
+++ pdfannotations/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  pdfannotations
+//
+//  Created by John Iversen on 9/26/11.
+//  Copyright 2011 technophobe-anodyne. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
