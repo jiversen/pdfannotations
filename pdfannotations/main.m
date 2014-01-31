bc88ece43e655147d8b1876c7af5b94efb378c18
John R Iversen <john@johniversen.org>
2014-01-30 21:30:48 -0800

    Write marked-up text. Extract note header line. Print header.  Tweak to text selection algorithm. Todo: embedded underlines sort by location on page [both will require us to more flexibly cache annotations + bounds--now they're just added to a string create rich text (attributed text) and place in clipboard--or for now, convert this markdown to rtf in some kind of automator droplet?

--- pdfannotations/main.m
+++ pdfannotations/main.m
@@ -23,16 +23,32 @@ int main (int argc, const char * argv[])
     
 
     if (argc < 2) {
-        fname = @"/Users/jri/Documents/dev/annotations/hilighttest.pdf";
-        //fname = @"/Users/jri/Documents/dev/annotations/Zarco.pdf";
+        fname = @"/Users/Shared/dev/Projects/annotations/hilighttest.pdf";
+        //fname = @"/Users/Shared/dev/Projects/annotations/Zarco.pdf";
+        //fname = @"/Users/jri/Documents/Papers2/Articles/Conway/Conway 2009 - The Importance of Sound for Cognitive Sequencing Abilities The Auditory Scaffolding Hypothesis - Curr Dir Psych Sci.pdf";
+        //fname = @"/blorg";
+        //fprintf(stdout, "Usage: %s fname.pdf\n", argv[0]);
+        //exit(1);
     } else {
         fname = [NSString stringWithUTF8String:argv[1]];
     }
 
     pdfDoc = [[PDFDocument alloc] initWithURL: [NSURL fileURLWithPath: fname ]];
+    if (pdfDoc == NULL) {
+        fprintf(stderr,"%s: %s appears not to be a valid pdf file.\n", argv[0], [fname UTF8String]);
+        exit(2);
+    }
+    
     NSUInteger nPage = [pdfDoc pageCount];
     //NSLog(@"%lu pages.",nPage);
-
+    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setTimeStyle:NSDateFormatterMediumStyle];
+    [dateFormatter setDateStyle:NSDateFormatterShortStyle];
+    //NSString *dft = [NSDateFormatter dateFormatFromTemplate:@"MM/dd/yyyy h:mm:ss a" options:0 locale:nil];
+    NSString *date = [dateFormatter stringFromDate:[NSDate date]];
+    NSString *header = [NSString stringWithFormat:@"%@\n\n%lu pages.\n\n%@\n\n", fname, [pdfDoc pageCount], date];
+    [header writeToFile:@"/dev/stdout" atomically:NO];
     
     for (int iPage=0; iPage<nPage; iPage++) {
         processPage([pdfDoc pageAtIndex:iPage]);
@@ -54,9 +70,10 @@ void processPage(PDFPage *aPage) {
     if ([annotations count] > 0) {
         pageText = [NSString stringWithFormat:@"# Page %@\n\n", [aPage label]];
         outputText = styledTextForAnnotations(annotations);
-        
-        [pageText   writeToFile:@"/dev/stdout" atomically:NO];
-        [outputText writeToFile:@"/dev/stdout" atomically:NO];
+        if ([outputText length] > 0) {
+            [pageText   writeToFile:@"/dev/stdout" atomically:NO];
+            [outputText writeToFile:@"/dev/stdout" atomically:NO];
+        }
     }
     
 
@@ -66,7 +83,10 @@ void processPage(PDFPage *aPage) {
 // find #quads/4, loop over quads, get text within each quad, concatenate
 // page points are bounds (1) + quadrilateralPoints
 // get contents
-// later: find underline within hilight
+// later: find underline within hilight by finding overlapping/contained quads
+// traverses in order annotations were defined (?) rather than based on page position
+//  heuristic: output from top to bottom, but this will fail for multi-column text, no?
+//  how is multi-column represented internally?
 
 // markdown styling, or attributed txt?
 NSString *styledTextForAnnotations(NSArray *annotations) {
@@ -87,7 +107,7 @@ NSString *styledTextForAnnotations(NSArray *annotations) {
 
     for (thisAnnotation in annotations) {
         annotationType = [thisAnnotation type];
-        annotationContents = [thisAnnotation contents];
+        annotationContents = [thisAnnotation contents]; //contains text of note or text box
         annotationPage = [thisAnnotation page];
         
         //markup, extract text
@@ -97,47 +117,87 @@ NSString *styledTextForAnnotations(NSArray *annotations) {
             //nQuads = [quadPoints count] / 4;
             bounds = [thisAnnotation bounds];
             start = [[quadPoints objectAtIndex:0] pointValue];
-            end = [[quadPoints objectAtIndex:[quadPoints count] - 3] pointValue];
+            end = [[quadPoints objectAtIndex:([quadPoints count] - 3)] pointValue];
             
+            //end = [[quadPoints objectAtIndex:[quadPoints count] - 1] pointValue];
+            
+            // to ensure we don't pick up extra lines, inset the start and end y values by 1/3 of line height
             midy = (start.y + [[quadPoints objectAtIndex:2] pointValue].y)/2;
             start.y = midy;
             
             midy = (end.y + [[quadPoints objectAtIndex:[quadPoints count] - 2] pointValue].y)/2;
             end.y = midy;
+            //end.y = 0;
             
             start.x += bounds.origin.x;
             start.y += bounds.origin.y;
             end.x += bounds.origin.x;
             end.y += bounds.origin.y;
             
+            // annotation text (hilighted or underlined) put in quotes
             sel = [annotationPage selectionFromPoint:start toPoint:end];
             annotationText = deHyphenate([sel string]);
+            annotationText = [NSString stringWithFormat:@"\"%@\"", annotationText]; // in quotes
             
             // underlined text becomes bold
             //  TODO: underline within hilight: if hilight, search annotations for
-            //  any underlines within it.
-            if (annotationType == @"Underline") {
-                annotationText = [NSString stringWithFormat:@"x %@ x", annotationText];
+            //  any underlines within it. Generally such an annotation will be created after the parent
+            //  so could check only previous.
+            if ([annotationType isEqualToString: @"Underline"]) {
+                //check if it falls within another annotation
+                annotationText = [NSString stringWithFormat:@"**%@**", annotationText];
             } 
             
-            // assemble annotation text & note. Could get fancy: if note has two paras,
+            // assemble annotation text & note (contents). Could get fancy: if note has two paras,
             //  make first a heading (only if begin with #?) and second a post-text comment:
             // ## fist para of comment (only if have > 1 para)
             // selection text
             //      remainder of comment
             //
-                        
-            [outputText appendFormat:@"%@\nText: %@\nContents: %@\n\n", 
-             annotationType, annotationText, annotationContents];
+            
+            NSArray *contentParts = [annotationContents componentsSeparatedByString:@"\n\n"];
+
+            switch ([contentParts count]) {
+                case 0: //no note
+                {
+                    [outputText appendFormat:@"%@\n\n", annotationText];
+                    break;
+                }
+                    
+                case 1: //simple comment, italicize
+                {
+                    NSString *note = [contentParts objectAtIndex:0];
+                    if ([note length] > 0) {
+                        [outputText appendFormat:@"%@\n\n\t_%@_\n\n", annotationText, note];
+                    } else {
+                        [outputText appendFormat:@"%@\n\n", annotationText];                        
+                    }
+                    break;
+                }
+                    
+                    
+                default: //comment w/ header
+                {
+                    NSMutableArray *parts = [NSMutableArray arrayWithArray:contentParts];
+                    NSString *hdr = [parts objectAtIndex:0];
+                    [parts removeObjectAtIndex:0];
+                    [outputText appendFormat:@"\n## %@\n\n%@\n\n\t_%@_\n\n", hdr, annotationText, [parts componentsJoinedByString:@"\n" ] ];
+                    break;
+                }
+            }
+
 
         // free text, direct
         } else if ([thisAnnotation isKindOfClass:[PDFAnnotationFreeText class]]) {
             if ([annotationContents length] > 0) {
-                [outputText appendFormat:@"%@: %@\n\n", annotationType, annotationContents];
+                [outputText appendFormat:@"\n_%@_\n\n", annotationContents];
             }
         }
     }
     
+    // sanitize text (force to UTF-8) incase we want to pipe to pandoc to render
+    //  rich text
+    outputText = [[NSString alloc] initWithData: [outputText dataUsingEncoding: NSASCIIStringEncoding allowLossyConversion:YES] encoding:NSASCIIStringEncoding];
     return outputText;
     
 }
